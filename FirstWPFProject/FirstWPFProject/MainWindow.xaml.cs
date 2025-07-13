5e933a8a0b342aa690e7a63765bdfd46c4127ef2
Paladincreator <gitivanzhutyaev@gmail.com>
2025-07-13 13:33:45 +0300

    hard task

--- FirstWPFProject/FirstWPFProject/MainWindow.xaml.cs
+++ FirstWPFProject/FirstWPFProject/MainWindow.xaml.cs
@@ -19,7 +19,30 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        button.Cursor=Cursors.Hand;
+        StackPanel stackPanel = new StackPanel();
+        Button squareButton = new Button() {Width = 100, Height = 100, HorizontalAlignment = HorizontalAlignment.Right, VerticalAlignment = VerticalAlignment.Center, Padding = new Thickness(0), VerticalContentAlignment = VerticalAlignment.Center };
+        Button squareButton1 = new Button()
+        {
+            Width = 50, Height = 50, HorizontalAlignment = HorizontalAlignment.Right,
+            VerticalAlignment = VerticalAlignment.Center, Padding = new Thickness(0),
+            VerticalContentAlignment = VerticalAlignment.Center
+        };
+        squareButton.Content = squareButton1;
+        stackPanel.Children.Add(squareButton);
+        StackPanel stackPanel1 = new StackPanel(){Orientation = Orientation.Vertical, Margin = new Thickness(0), HorizontalAlignment = HorizontalAlignment.Left, VerticalAlignment = VerticalAlignment.Center, Width = 300, Height = 300, Background = new SolidColorBrush(Color.FromRgb(155, 255, 255))};
+
+        Button squareButton3 = new Button() {Width = 100, Height = 100, HorizontalAlignment = HorizontalAlignment.Left, VerticalAlignment = VerticalAlignment.Center, Padding = new Thickness(0), VerticalContentAlignment = VerticalAlignment.Center };
+
+        stackPanel1.Children.Add(squareButton3);
+        stackPanel.Children.Add(stackPanel1);
+        window.AddChild(stackPanel);
+        
+        
+        
+
+
     }
+    
+    
     
 }
